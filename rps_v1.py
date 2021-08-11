1a924b89b0e9e8c0ea6aca9d7f681e74e5be28db
Noah <noahvar23@gmail.com>
2021-08-11 16:18:02 -0400

    Moved rps_list inside rockpaperscissors()

--- rps_v1.py
+++ rps_v1.py
@@ -1,8 +1,7 @@
 import random
 
-rps_list = ["rock", "paper", 'scissors']
-
 def rockpaperscissors():
+    rps_list = ["rock", "paper", 'scissors']
 
     c = (str(input("Rock Paper or Scissors? ")))
     ai = random.choice(rps_list)
