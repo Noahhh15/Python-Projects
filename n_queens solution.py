c0d5a817d9346e13986c789e0df8373233168a5e
Noah <noahvar23@gmail.com>
2021-08-12 11:29:32 -0400

    Added problem desc. and moved lines

--- n_queens solution.py
+++ n_queens solution.py
@@ -1,16 +1,23 @@
 """
-This function returns a boolean value True if it is safe to place a queen there
-considering the current state of the board.
-Parameters :
-board(2D matrix) : board
-row ,column : coordinates of the cell on a board
-Returns :
-Boolean Value
+ The nqueens problem is of placing N queens on a N * N
+ chess board such that no queen can attack any other queens placed
+ on that chess board.
+ This means that one queen cannot have any other queen on its horizontal, vertical and
+ diagonal lines.
 """
-
 solution = []
 
 def isSafe(board, row, column):
+    """
+    This function returns a boolean value True if it is safe to place a queen there
+    considering the current state of the board.
+    Parameters :
+    board(2D matrix) : board
+    row ,column : coordinates of the cell on a board
+    Returns :
+    Boolean Value
+    """
+
     for i in range (len(board)):
         if board[row][i] == 1:
             return False
